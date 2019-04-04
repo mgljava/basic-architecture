781ce38831ba4a04388793846fcfa99b042084e4
Minggl <guangliang1120@gmail.com>
2019-04-04 11:09:31 +0800

    init project

---
+++ src/main/java/com/github/mgljava/basicarchitecture/BasicArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.github.mgljava.basicarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mgljava/basicarchitecture/BasicArchitectureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.mgljava.basicarchitecture;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicArchitectureApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
